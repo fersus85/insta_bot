b6f0388adef4600aca066b42c8950f4262cd861d
fersus <deriabin_85@mail.ru>
2023-08-22 10:55:32 +0300

    Create pytest fixtures & add test for login.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,31 @@
 import pytest
+import os
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.options import Options
+
+from webdriver_manager.chrome import ChromeDriverManager
+
+from dotenv import load_dotenv
+from pathlib import Path
 
 
-# @pytest.fixture
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+name = os.getenv('NAME')
+psw = os.getenv('PSW')
+
+
+@pytest.fixture
+def get_driver():
+    options = Options()
+    options.add_argument("--headless=new")
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+    return driver
+
+
+@pytest.fixture
+def get_credentials():
+    return (name, psw)
--- tests/unit_tests/test_insta_bot.py
+++ tests/unit_tests/test_insta_bot.py
@@ -1,6 +1,35 @@
-import requests
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
-def test_login():
-    response = requests.get('https://www.google.com/')
-    print(response)
+def test_login(get_driver, get_credentials):
+    name, psw = get_credentials
+    DASHBOARD_URL = 'https://www.instagram.com/'
+    URL_AFTER_AUTH = "https://www.instagram.com/accounts/onetap/?next=%2F"
+    try:
+        get_driver.implicitly_wait(10)
+        get_driver.get('https://www.instagram.com/')
+
+        assert get_driver.current_url == DASHBOARD_URL
+
+        u_name_input = get_driver.find_element(By.NAME, "username")
+        u_name_input.clear()
+        u_name_input.send_keys(name)
+       
+        psw_input = get_driver.find_element(By.NAME, "password")
+        psw_input.clear()
+        psw_input.send_keys(psw)
+
+        psw_input.send_keys(Keys.ENTER)
+
+        time.sleep(10)
+
+        assert get_driver.current_url == URL_AFTER_AUTH
+
+        get_driver.close()
+        get_driver.quit()
+
+    except Exception as ex:
+        get_driver.close()
+        get_driver.quit()
