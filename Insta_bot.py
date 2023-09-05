5cd7690b832d14231c59ef96bf955b994ab3a5d5
fersus <deriabin_85@mail.ru>
2023-09-05 18:08:54 +0300

    Describe functions, refactor def is_xpath_exist and def download_img

--- Insta_bot.py
+++ Insta_bot.py
@@ -41,11 +41,13 @@ class InstagramBot:
 
         
     def close_browser(self):
+        ''' close webdriver '''
         self.driver.close()
         self.driver.quit()
 
 
     def login_collect_cookie(self) -> None:
+        ''' Login in instagram account with usrname and psw, collect cookie '''
         try:
             self.driver.implicitly_wait(10)
             self.driver.get('https://www.instagram.com/')
@@ -69,6 +71,7 @@ class InstagramBot:
 
 
     def login_with_cookie(self, headless: bool=False) -> None:
+        ''' Login in instagram account if you have cookie '''
         if headless:
             options = Options()
             options.add_argument("--headless=new")
@@ -94,6 +97,7 @@ class InstagramBot:
 
     def collect_posts_user(self, user: str, scroll: int=4,
                             save_to_file: bool = False) -> Optional[list]:
+        ''' save user's posts in file or return list with its '''
         self.driver.implicitly_wait(10)
         try:
             self.driver.get(f'https://www.instagram.com/{user}/')
@@ -113,6 +117,7 @@ class InstagramBot:
             print(ex)
 
     def likes_on_post(self, posts: list) -> None:
+        ''' press like button on posts '''
         self.driver.implicitly_wait(10)
         for post in posts[0:3]:
             try:
@@ -124,52 +129,41 @@ class InstagramBot:
                 print(ex)
                 self.close_browser()
 
-    def is_xpath_exist(self, xpath):
-        # inspect is exist xpath on a page
-        driver = self.driver
-        try:
-            driver.find_element(By.XPATH, xpath)
-            exist = True
-        except NoSuchElementException:
-            exist = False
-        return exist
+    def _is_xpath_exist(self, xpath):
+        ''' inspect is exist xpath on a page '''
+        return bool(self.driver.find_elements(By.XPATH, xpath))
 
-    def download_img(self, userpage, scroll=2):
-        driver = self.driver
-        driver.find_element(
-            By.CSS_SELECTOR, 'svg[aria-label="Поисковый запрос"]').click()
-        input_requests = driver.find_element(
-            By.CSS_SELECTOR, "input[aria-label='Ввод поискового запроса'")
-        input_requests.clear()
-        input_requests.send_keys(userpage)
-        time.sleep(5)
-        input_requests.send_keys(Keys.ENTER)
-        time.sleep(7)
-        input_requests.send_keys(Keys.ENTER)
-        time.sleep(5)
-        # Scroll down if you need
-        for i in range(1, scroll):
-            driver.execute_script(
-                'window.scrollTo(0, document.body.scrollHeight);')
-            time.sleep(random.randrange(7, 10))
-        # Collect all reference from page
-        links = driver.find_elements(By.TAG_NAME, 'img')
-        images = []
-        # Filter post's url
-        for item in links:
-            links = item.get_attribute('src')
-            images.append(links)
-        # print(images)
-        print(len(images))
-        driver.implicitly_wait(5)
-        path = os.getcwd()
-        path = os.path.join(path, userpage)
-        os.mkdir(path)
-        counter = 0
-        for image in images:
-            save_as = (os.path.join(path, userpage + str(counter) + '.jpg'))
-            wget.download(image, save_as)
-            counter += 1
+
+    def download_img(self, user: str, scroll: int=2) -> None:
+        ''' download images from instagram page '''
+        self.driver.implicitly_wait(10)
+        try:
+            self.driver.get(f'https://www.instagram.com/{user}/')
+            for i in range(1, scroll):
+                self.driver.execute_script(
+                    'window.scrollTo(0, document.body.scrollHeight);')
+                time.sleep(random.randrange(3, 7))
+
+            links = self.driver.find_elements(By.TAG_NAME, 'img')
+            for el in links:
+                print(el.get_attribute('value'))
+                print(el.__dict__())
+                # print(el.accessible_name, el.aria_role)
+            images = [item.get_attribute('src') for item in links]
+            # print(images[0])
+    
+            path = os.getcwd()
+            path = os.path.join(path, user)
+            os.mkdir(path)
+            counter = 0
+            for image in images:
+                save_as = (os.path.join(path, user + str(counter) + '.jpg'))
+                wget.download(image, save_as)
+                print('ok')
+                counter += 1
+        except Exception as ex:
+            print(ex)
+        
 
     def grab_followers(self, userpage, numb_iter):
         driver = self.driver
@@ -263,6 +257,7 @@ class InstagramBot:
 
 bot = InstagramBot(username=name, password=psw)
 bot.login_with_cookie()
-posts = bot.collect_posts_user('explor1r')
-bot.likes_on_post(posts)
+# posts = bot.collect_posts_user('explor1r')
+# bot.likes_on_post(posts)
+bot.download_img('explor1r')
 bot.close_browser()
