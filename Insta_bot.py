1bd31104959ee27e4c598dba19c9df7e15db5d99
fersus <deriabin_85@mail.ru>
2023-09-07 20:08:55 +0300

    Apgrade clean href in grab followers, refactor def like and grab

--- Insta_bot.py
+++ Insta_bot.py
@@ -187,14 +187,21 @@ class InstagramBot:
         
         links = self.driver.find_elements(By.TAG_NAME, 'a')
 
-        hrefs = set(
-            [link.get_attribute('href') 
-             for link in links if '/p/' not in link.get_attribute('href')]
-             )
-        clean_hrefs = re.findall(r'\w{5}\W{3}\w+.\w+.\w+\/\w+\/', hrefs)
+        hrefs = [link.get_attribute('href') 
+                 for link in links if '/p/' not in link.get_attribute('href')]
+             
+        clean_hrefs = set(
+            re.findall(r'\w{5}\W{3}\w+.\w+.\w+\/\w+\/', '\n'.join(hrefs))
+        )
+        tech_links = ['https://www.instagram.com/direct/', 'https://about.instagram.com/blog/',
+                      'https://about.meta.com/technologies/', 'https://about.meta.com/technologies/',
+                      'https://www.instagram.com/web/', 'https://www.instagram.com/reels/',
+                      'https://developers.facebook.com/docs/', 'https://www.instagram.com/about/'
+                      ]
         with open(f"{user}'s followers.txt", 'w') as file:
             for href in clean_hrefs:
-                file.write(href + '\n')
+                if href not in tech_links:
+                    file.write(href + '\n')
 
     def follow_and_like(self, filename: str) -> None:
         ''' follow by users from file and press like on their posts ''' 
@@ -205,33 +212,25 @@ class InstagramBot:
             try:
                 for link in links:
                     self.driver.get(link)
-                    self.driver.find_element(
-                        By.CLASS_NAME, "_acan _acap _acas _aj1-").click()
-                    posts = self._collect_posts()
-                    self.likes_on_post(posts)
-                    # links = driver.find_elements(By.TAG_NAME, 'a')
-                        # posts = []
-                        # # Filter post's url
-                        # for item in links:
-                        #     links = item.get_attribute('href')
-                        #     if '/p/' in links:
-                        #         posts.append(links)
-                        # print(len(posts))
-                        # numbers_likes = random.randrange(2, 5)
-                        # for post in posts[0:numbers_likes]:
-                        #     driver.get(post)
-                        #     # Press like button
-                        #     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,
-                        #                                                                 '/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/section/main/div[1]/div[1]/article/div/div[2]/div/div[2]/section[1]/span[1]/button'))).click()
-                        #     time.sleep(random.randrange(3, 5))
+                    xpath = "//div[contains(text(),'Follow')]"
+                    if self._is_xpath_exist(xpath):
+                        self.driver.find_element(
+                            By.XPATH, xpath).click()
+                        time.sleep(random.randrange(2, 4))
+                        posts = self._collect_posts()
+                        time.sleep(random.randrange(2, 4))
+                        self.likes_on_post(posts)
+                    else:
+                        print('no xpass')
                         
             except Exception as ex:
                 print(ex)
 
 
 bot = InstagramBot(username=NAME, password=PSW)
-bot.login_with_cookie(headless=True)
-bot.grab_followers(user='explor1r')
+bot.login_with_cookie()
+bot.follow_and_like("explor1r's followers.txt")
+# bot.grab_followers(user='explor1r')
 # posts = bot.collect_posts_user('explor1r')
 # bot.likes_on_post(posts)
 # bot.download_img('explor1r')
